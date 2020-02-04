53b36ae170bba12e8d999ff11e47706a41164872
commanday <60590616+commanday@users.noreply.github.com>
2020-02-04 11:26:25 +0800

    Update information.js

--- JavaScript source code/information.js
+++ JavaScript source code/information.js
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>
+function myFunction(){
+    alert("hello！");
+}
+</script>
+</head>
+<body>
 
+<input type="button" onclick="myFunction()" value="button" />
+
+</body>
+</html>
