141bcaa22dac786d46c32b5bbc51150550cdb5c4
commanday <60590616+commanday@users.noreply.github.com>
2020-02-04 11:38:05 +0800

    Create time1.js

---
+++ JavaScript source code/time1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>菜鸟教程(runoob.com)</title>
+</head>
+<body>
+
+<script>
+var d=new Date();
+document.write(d);
+</script>
+
+</body>
+</html>
