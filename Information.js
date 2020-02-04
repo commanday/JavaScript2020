096984035c038c69af4389a32e823365ac160d50
commanday <60590616+commanday@users.noreply.github.com>
2020-02-04 11:23:05 +0800

    Create Information.js

---
+++ Information.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>
+function myFunction(){
+    alert("hello！");
+}
+</script>
+</head>
+<body>
+
+<input type="button" onclick="myFunction()" value="显示警告框" />
+
+</body>
+</html>
